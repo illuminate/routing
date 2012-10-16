be88cdf0b3c5c785e9eb7df1710b9c0ca2f2de25
Taylor Otwell <taylorotwell@gmail.com>
2012-10-15 23:11:53 -0500

    more tests.

--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Mockery as m;
+use Symfony\Component\HttpFoundation\Response;
 
 class ControllerTest extends PHPUnit_Framework_TestCase {
 
@@ -14,18 +15,32 @@ class ControllerTest extends PHPUnit_Framework_TestCase {
 	{
 		$controller = new BasicControllerStub;
 		$container = new Illuminate\Container;
-		$container['filter.parser'] = $container->share(function()
-		{
-			return m::mock('StdClass');
-		});
+		$container['filter.parser'] = $container->share(function() { return m::mock('StdClass'); });
 		$container['filter.parser']->shouldReceive('parse')->andReturn(array());
 		$router = m::mock('Illuminate\Routing\Router');
 		$router->shouldReceive('getRequest')->andReturn(m::mock('Symfony\Component\HttpFoundation\Request'));
 		$router->shouldReceive('getCurrentRoute')->andReturn(m::mock('Illuminate\Routing\Route'));
-		$router->shouldReceive('prepare')->once()->andReturnUsing(function($response, $request) { return new Symfony\Component\HttpFoundation\Response($response); });
+		$router->shouldReceive('prepare')->once()->andReturnUsing(function($response, $request) { return new Response($response); });
 
 		$response = $controller->callAction($container, $router, 'basicAction', array('foo'));
 		$this->assertEquals('foo', $response->getContent());
+	}
+
+
+	public function testBeforeFiltersAreCalledAndHaltRequestLifecycle()
+	{
+		$controller = new BasicControllerStub;
+		$container = new Illuminate\Container;
+		$container['filter.parser'] = $container->share(function() { return m::mock('StdClass'); });
+		$container['filter.parser']->shouldReceive('parse')->twice()->andReturn(array('foo-filter'), array());
+		$router = m::mock('Illuminate\Routing\Router');
+		$router->shouldReceive('getRequest')->andReturn($request = m::mock('Symfony\Component\HttpFoundation\Request'));
+		$router->shouldReceive('getCurrentRoute')->andReturn($route = m::mock('Illuminate\Routing\Route'));
+		$route->shouldReceive('callFilter')->once()->with('foo-filter', $request)->andReturn('filtered!');
+		$router->shouldReceive('prepare')->once()->andReturnUsing(function($response, $request) { return new Response($response); });
+
+		$response = $controller->callAction($container, $router, 'basicAction', array('foo'));
+		$this->assertEquals('filtered!', $response->getContent());
 	}
 
 }
