f743b396dbdf228c32170fa82291bca16fd88c50
Frank Mayer <frank@frankmayer.net>
2012-12-20 03:53:01 +0200

    Implemented HTTP PATCH method and assigned it to resourceful method 'update'. Assigned HTTP PUT method to new resourceful method 'replace'
    Changed tests accordingly. (Just method count tests inside RoutingTest.php)
    
    Signed-off-by: Frank Mayer <frank@frankmayer.net>

--- tests/RoutingTest.php
+++ tests/RoutingTest.php
@@ -65,7 +65,7 @@ class RoutingTest extends PHPUnit_Framework_TestCase {
 		$router->resource('foo', 'FooController');
 		$routes = $router->getRoutes();
 
-		$this->assertEquals(7, count($routes));
+		$this->assertEquals(8, count($routes));
 
 		$router = new Router;
 		$router->resource('foo', 'FooController', array('only' => array('show', 'destroy')));
@@ -77,7 +77,7 @@ class RoutingTest extends PHPUnit_Framework_TestCase {
 		$router->resource('foo', 'FooController', array('except' => array('show', 'destroy')));
 		$routes = $router->getRoutes();
 
-		$this->assertEquals(5, count($routes));
+		$this->assertEquals(6, count($routes));
 	}
 
 
