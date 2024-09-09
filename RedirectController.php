d6beec908000ecad8983c4608e4037513505321b
Ahmed AbdElRazek <razek4214@gmail.com>
2024-09-09 17:05:23 +0300

    Fix parameter handling in route redirection
    Resolves an error that occurs when route redirection is attempted with malformed parameter inputs. By trimming braces from parameters, it ensures that route variables are correctly parsed, preventing the "Missing required parameter" error.

--- RedirectController.php
+++ RedirectController.php
@@ -31,7 +31,10 @@ class RedirectController extends Controller
 
         $parameters = $parameters->only(
             $route->getCompiled()->getPathVariables()
-        )->all();
+        )->map(function ($value) {
+            return trim($value, '{}');
+        })->all();
+
 
         $url = $url->toRoute($route, $parameters, false);
 
